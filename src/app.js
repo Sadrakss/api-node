f6292079c196dd7d77195460ac8cdeab951b1812
sadrakss <sadrakss@pop-os.localdomain>
2020-02-07 15:47:55 -0300

    connection db ready

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const router = express.Router()
+
+// connect db
+mongoose.connect('mongodb+srv://api:api@dbs-5o4eu.mongodb.net/test?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(() => {
+    console.log('connected to mongodb')
+}).catch((err) => {
+    console.log('connection error!' + err)
+})
+
 
 // loads the routes
 const indexRoute = require('./routes/index-route')
